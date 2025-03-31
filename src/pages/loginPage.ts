738925f958fc6d562a6f16c4af83c8eeaff7af58
LeoNerdoG <leonardoga@payoneer.com>
2025-03-31 23:13:46 +0200

    Small changes to code, added wait before checking for myAccount link
    Signed-off-by: LeoNerdoG <leonardoga@payoneer.com>

--- src/pages/loginPage.ts
+++ src/pages/loginPage.ts
@@ -13,41 +13,4 @@ export class LoginPage {
     await this.driver.findElement(LoginPageElements.passwordField).sendKeys(password);
     await this.driver.findElement(LoginPageElements.loginButton).click();
   }
-
-  // async enterEmail(email: string): Promise<void> {
-  //   const emailInput = await this.driver.findElement(LoginPageElements.emailField);
-  //   await emailInput.sendKeys(email);
-  // }
-
-  // async enterPassword(password: string): Promise<void> {
-  //   const passwordInput = await this.driver.findElement(LoginPageElements.passwordField);
-  //   await passwordInput.sendKeys(password);
-  // }
-
-  // async clickLoginButton(): Promise<void> {
-  //   const loginButton = await this.driver.findElement(By.css('input[value="Log in"]'));
-  //   await loginButton.click();
-  // }
-
-  // async getLoginErrorMessage(): Promise<string> {
-  //   try {
-  //     const errorMessage = await this.driver.findElement(By.css(".message-error.validation-summary-errors")).getText();
-  //     return errorMessage;
-  //   } catch(e){
-  //       return "";
-  //   }
-  // }
-
-  // async getLogoutLink(): Promise<WebElement> {
-  //     return this.driver.findElement(By.linkText("Log out"));
-  // }
-
-  // async clickLogout(): Promise<void> {
-  //     const logoutLink = await this.getLogoutLink();
-  //     await logoutLink.click();
-  // }
-
-  // async getLoggedInEmail(): Promise<string> {
-  //     return this.driver.findElement(By.css(".account")).getText();
-  // }
 }
